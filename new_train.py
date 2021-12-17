8955e0c57d4c99af9704d62295ee97e73d43f504
zhaomeiyong <zhaomeiyong@bytedance.com>
2021-12-17 12:01:38 +0800

    feat: update
    Change-Id: Icde48a841d5a44c2ab8b55e29e11715ed34509c8

---
+++ new_train.py
@@ -0,0 +1,86 @@
+# Copyright (c) 2019-present, HuggingFace Inc.
+# All rights reserved. This source code is licensed under the BSD-style license found in the LICENSE file in the root directory of this source tree.
+import logging
+import random
+from pprint import pformat
+from argparse import ArgumentParser
+
+import numpy as np
+import torch
+from work.trainer import Trainer
+
+
+logger = logging.getLogger(__file__)
+
+
+def setup_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+
+
+setup_seed(2019)
+
+
+def train():
+    parser = ArgumentParser()
+    parser.add_argument('--gpt2', action='store_true', help="use gpt2")
+    parser.add_argument("--model_checkpoint", type=str, default="config/cgpt/", help="Path or URL of the model")
+    parser.add_argument("--from_step", type=int, default=-1, help="Init learning rate from this step")
+    parser.add_argument('--pretrained', action='store_true', help="If False train from scratch")
+    parser.add_argument("--data_path", type=str, default="",
+                        help="Path or url of the dataset. ")
+    parser.add_argument("--train_path", type=str, default="data/toy_train.txt",
+                        help="Path of the train dataset for dist dataset. ")
+    parser.add_argument("--valid_path", type=str, default="data/toy_valid.txt",
+                        help="Path of the valid dataset for dist dataset. ")
+    parser.add_argument("--dataset_cache", type=str, default="dataset_cache",
+                        help="Path or url of the dataset cache")
+    parser.add_argument('--log_file', '-log_file', type=str, default="", help="Output logs to a file under this path")
+    parser.add_argument("--num_workers", type=int, default=8, help="Number of subprocesses for data loading")
+    parser.add_argument("--n_epochs", type=int, default=70, help="Number of training epochs")
+    parser.add_argument("--train_batch_size", type=int, default=2, help="Batch size for training")
+    parser.add_argument("--valid_batch_size", type=int, default=2, help="Batch size for validation")
+    parser.add_argument("--max_history", type=int, default=15, help="Number of previous exchanges to keep in history")
+    parser.add_argument("--scheduler", type=str, default="noam", choices=['noam', 'linear'], help="method of optim")
+    parser.add_argument("--n_emd", type=int, default=768, help="Number of n_emd in config file (for noam)")
+    parser.add_argument("--lr", type=float, default=5e-5, help="Learning rate")
+    parser.add_argument("--eval_before_start", action='store_true',
+                        help="If true start with a first evaluation before training")
+    parser.add_argument("--warmup_steps", type=int, default=5000, help="Warm up steps")
+    parser.add_argument("--valid_steps", type=int, default=5000, help="Perfom validation every X steps")
+    parser.add_argument("--gradient_accumulation_steps", type=int, default=64,
+                        help="Accumulate gradients on several steps")
+    parser.add_argument("--max_norm", type=float, default=1.0, help="Clipping gradient norm")
+    parser.add_argument("--device", type=str, default="cuda" if torch.cuda.is_available() else "cpu",
+                        help="Device (cuda or cpu)")
+    parser.add_argument("--fp16", type=str, default="",
+                        help="Set to O0, O1, O2 or O3 for fp16 training (see apex documentation)")
+    parser.add_argument("--local_rank", type=int, default=-1,
+                        help="Local rank for distributed training (-1: not distributed)")
+    args = parser.parse_args()
+
+    # logging is set to INFO (resp. WARN) for main (resp. auxiliary) process.
+    # logger.info => log main process only, logger.warning => log all processes
+    logging.basicConfig(level=logging.INFO if args.local_rank in [-1, 0] else logging.WARN)
+    logger.warning("Running process %d", args.local_rank)
+    logger.info("Arguments: %s", pformat(args))
+
+    trainer = Trainer(args, logger)
+    trainer.train()
+
+    # trainer.run(train_loader, max_epochs=args.n_epochs)
+    #
+    # # On the main process: close tensorboard logger and rename the last checkpoint
+    # # (for easy re-loading with OpenAIGPTModel.from_pretrained method)
+    # if args.local_rank in [-1, 0] and args.n_epochs > 0:
+    #     os.rename(checkpoint_handler._saved[-1][1][-1],
+    #               os.path.join(tb_logger.writer.logdir,
+    #                            WEIGHTS_NAME))  # TODO: PR in ignite to have better access to saved file paths (cleaner)
+    #     tb_logger.close()
+
+
+if __name__ == "__main__":
+    train()
--- work/trainer.py
+++ work/trainer.py
@@ -1,110 +1,178 @@
 import torch
 import os
-import random
-import torch.nn as nn
 import torch.distributed
-import torch.nn.functional as F
-import math
 import torch.tensor
-from torch.utils.data import DataLoader
-from torch.utils.tensorboard import SummaryWriter
+import math
+from pprint import pformat
+from torch.nn import CrossEntropyLoss
+from torch.nn.parallel import DistributedDataParallel
 from torch.optim.lr_scheduler import LambdaLR
-from tqdm import tqdm
-import tqdm
-import json
 from transformers import (OpenAIGPTLMHeadModel, OpenAIGPTConfig, GPT2LMHeadModel, GPT2Config,
                           WEIGHTS_NAME, CONFIG_NAME, AdamW, BertTokenizer)
 
 from od.inputters.inputter import build_dataloaders, build_dist_loaders
-from evaluator import Evaluator
+from tqdm.autonotebook import tqdm
 
 
 class Trainer(object):
-    def __init__(self, logger, log_dir, conf, conf_path, distributed, device=torch.device('cuda')):
+    def __init__(self, args, logger):
         self.logger = logger
-        self.log_dir = log_dir
-        self.conf = conf
-        self.conf_path = conf_path
-        self.device = device
 
-        model_class = OpenAIGPTLMHeadModel if not conf.gpt2 else GPT2LMHeadModel
-        config_class = OpenAIGPTConfig if not conf.gpt2 else GPT2Config
+        # Initialize distributed training if needed
+        args.distributed = (args.local_rank != -1)
+        if args.distributed:
+            torch.cuda.set_device(args.local_rank)
+            args.device = torch.device("cuda", args.local_rank)
+            torch.distributed.init_process_group(backend='nccl', init_method='env://')
+
+        self.logger.info("Prepare tokenizer, pretrained model and optimizer - add special tokens for fine-tuning")
+        model_class = OpenAIGPTLMHeadModel if not args.gpt2 else GPT2LMHeadModel
+        config_class = OpenAIGPTConfig if not args.gpt2 else GPT2Config
         tokenizer_class = BertTokenizer
-        if conf.pretrained:
-            tokenizer = tokenizer_class.from_pretrained(conf.model_checkpoint, do_lower_case=True,
+        if args.pretrained:
+            tokenizer = tokenizer_class.from_pretrained(args.model_checkpoint, do_lower_case=True,
                                                         never_split=["[speaker1]", "[speaker2]"])
-            self.model = model_class.from_pretrained(conf.model_checkpoint)
+            self.model = model_class.from_pretrained(args.model_checkpoint)
         else:
-            tokenizer = tokenizer_class(os.path.join(conf.model_checkpoint, "vocab.txt"), do_lower_case=True,
+            tokenizer = tokenizer_class(os.path.join(args.model_checkpoint, "vocab.txt"), do_lower_case=True,
                                         never_split=["[speaker1]", "[speaker2]"])
-            config = config_class.from_json_file(os.path.join(conf.model_checkpoint, CONFIG_NAME))
-            model = model_class(config)
-        self.model.to(device)
+            config = config_class.from_json_file(os.path.join(args.model_checkpoint, CONFIG_NAME))
+            self.model = model_class(config)
+        self.model.to(args.device)
 
-        self.rank = torch.distributed.get_rank() if distributed else -1
-        self.optimizer = AdamW([{'params': self.model.parameters(), 'initial_lr': self.conf.lr}], lr=self.conf.lr, correct_bias=True)
+        self.optimizer = AdamW([{'params': self.model.parameters(), 'initial_lr': args.lr}], lr=args.lr, correct_bias=True)
 
-        loader_class = build_dist_loaders if not conf.data_path else build_dataloaders
-        self.train_loader, self.val_loader, self.train_sampler, self.valid_sampler = loader_class(conf, tokenizer, logger)
+        logger.info("Prepare datasets")
+        loader_class = build_dist_loaders if not args.data_path else build_dataloaders
+        self.train_loader, self.val_loader, self.train_sampler, self.valid_sampler = loader_class(args, tokenizer, logger)
 
-        self.evaluator = Evaluator(args=self.conf, valid_sampler=self.valid_sampler, data=self.train_loader, model=self.model, logdir=self.log_dir)
+        # Prepare model for FP16 and distributed training if needed (order is important, distributed should be the last)
+        if args.fp16:
+            from apex import amp  # Apex is only required if we use fp16 training
+            self.model, optimizer = amp.initialize(self.model, self.optimizer, opt_level=args.fp16)
+        if args.distributed:
+            self.model = DistributedDataParallel(self.model, device_ids=[args.local_rank], output_device=args.local_rank)
 
-    def _eval_train(self, epoch, lr_scheduler):
-        if not (self.rank == -1 or self.rank == 0):
-            iter = self.train_loader
+        self.conf = args
+
+        model_size = self.conf.n_emd
+        noam_lambda = lambda step: (
+                model_size ** (-0.5) * min((step + 1) ** (-0.5), (step + 1) * self.conf.warmup_steps ** (-1.5)))
+        self.lr_scheduler = LambdaLR(self.optimizer, lr_lambda=noam_lambda, last_epoch=self.conf.from_step)
+        self.milestones = [0, args.n_epochs * len(self.train_loader)]
+        self.values = [args.lr, 0.0]
+        self.event_index = 0
+
+        self.metrics = {}
+        self.pbar = None
+
+    def process(self, iteration, batch):
+        input_ids, token_type_ids, lm_labels = tuple(input_tensor.to(self.conf.device) for input_tensor in batch)
+        self.model.train()
+        (lm_loss), *_ = self.model(input_ids, labels=lm_labels, token_type_ids=token_type_ids)
+        loss = lm_loss / self.conf.gradient_accumulation_steps
+        if self.conf.fp16:
+            from apex import amp
+            with amp.scale_loss(loss, self.optimizer) as scaled_loss:
+                scaled_loss.backward()
+            torch.nn.utils.clip_grad_norm_(amp.master_params(self.optimizer), self.conf.max_norm)
         else:
-            iter = tqdm.tqdm(self.train_loader, total=len(self.train_loader), dynamic_ncols=True)
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.conf.max_norm)
+        if iteration % self.conf.gradient_accumulation_steps == 0:
+            self.optimizer.step()
+            self.optimizer.zero_grad()
 
-        loss_ans = None
-        lr_vaue = None
-        for i, data in iter:
-            if i % self.conf.valid_steps == 0:
-                self.evaluator.run()
+        self.metrics['loss'] = loss.item()
+        self.metrics['lr'] = self.optimizer.param_groups[0]['lr']
 
-            lr_scheduler.last_epoch = epoch
-            lr_list = lr_scheduler.get_lr()
-            value = lr_list[0]
-            for param_group in self.optimizer.param_groups:
-                param_group['lr'] = value
-
-            input_ids, token_type_ids, lm_labels = tuple(input_tensor.to(self.device) for input_tensor in data)
-            self.model.train()
-            (lm_loss), *_ = self.model(input_ids, labels=lm_labels, token_type_ids=token_type_ids)
-            loss = lm_loss / self.conf.gradient_accumulation_steps
-            if self.conf.fp16:
-                from apex import amp
-                with amp.scale_loss(loss, self.optimizer) as scaled_loss:
-                    scaled_loss.backward()
-                torch.nn.utils.clip_grad_norm_(amp.master_params(self.optimizer), self.conf.max_norm)
-            else:
-                loss.backward()
-                torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.conf.max_norm)
-            if i % self.conf.gradient_accumulation_steps == 0:
-                self.optimizer.step()
-                self.optimizer.zero_grad()
-
-            # todo checkpoint
-            loss_ans = loss.item()
-            lr_vaue = self.optimizer.param_groups[0]['lr']
-
-        return loss_ans, lr_vaue
-
-    def train(self, start_epoch, epochs, after_epoch_funcs, after_step_funcs):
-        self.evaluator.run(self.val_loader)
-        epoch = start_epoch + 1
-        while epoch < epochs:
-            # todo 考虑下优化器是否需要再封装
-            self.logger.info('Training on process {}, epoch {}, step {}'.format(
-                self.rank, epoch, self.optimizer.step()))
-            self.train_sampler.set_epoch(epoch)
-
-            model_size = self.conf.n_emd
-            noam_lambda = lambda step: (
-                    model_size ** (-0.5) * min((step + 1) ** (-0.5), (step + 1) * self.conf.warmup_steps ** (-1.5)))
-            noam_scheduler = LambdaLR(self.optimizer, lr_lambda=noam_lambda, last_epoch=self.conf.from_step)
-
-            loss, lr_value = self._eval_train(epoch, noam_scheduler)
-            self.evaluator.run(self.val_loader)
-            # todo checkpoint
+    def train(self):
+        self.pbar = tqdm(total=len(self.train_loader), leave=True,
+                         bar_format='{desc}[{n_fmt}/{total_fmt}] {percentage:3.0f}%|{bar}{postfix} [{elapsed}<{remaining}]',
+                         mininterval=2)
+        if self.conf.eval_before_start:
+            self.evaluator(self.val_loader, 0)
+        epoch = 0
+        while epoch < self.conf.n_epochs:
             epoch += 1
+            if self.conf.distributed:
+                self.train_sampler.set_epoch(epoch)
 
+            iteration = 0
+            for batch in self.train_loader:
+                iteration += 1
+
+                if iteration % self.conf.valid_steps == 0:
+                    self.evaluator(self.val_loader, epoch)
+
+                if self.conf.scheduler != "linear":
+                    self.lr_scheduler.last_epoch = epoch
+                    lr_list = self.lr_scheduler.get_lr()
+                    value = lr_list[0]
+                else:
+                    if self.milestones[0] > self.event_index:
+                        start_index, end_index, start_value, end_value = self.event_index - 1, self.event_index, self.values[0], self.values[0]
+                    elif self.milestones[1] <= self.event_index:
+                        start_index, end_index, start_value, end_value = self.event_index, self.event_index + 1, self.values[1], self.values[1]
+                    else:
+                        start_index, end_index, start_value, end_value = self.milestones[0], self.milestones[1], self.values[0], self.values[1]
+
+                    value = start_value + (end_value - start_value) * (self.event_index - start_index) / (
+                                end_index - start_index)
+                    self.event_index += 1
+
+                for param_group in self.optimizer.param_groups:
+                    param_group['lr'] = value
+                self.process(iteration, batch)
+
+                self.pbar.set_description("Epoch [{}/{}]".format(epoch, self.conf.n_epochs))
+                self.pbar.set_postfix(**{'loss': self.metrics['loss'], 'lr': self.metrics['lr']})
+                self.pbar.update()
+                # todo 打印日志
+
+            self.evaluator(self.val_loader, epoch)
+            self.pbar.close()
+            # todo 打印日志
+            # todo 保存模型
+        if self.conf.n_epochs < 1:
+            self.evaluator(self.val_loader, self.conf.n_epochs)
+
+
+
+    def average_distributed_scalar(self, scalar, args):
+        """ Average a scalar over the nodes if we are in distributed training. We use this for distributed evaluation. """
+        if self.conf.local_rank == -1:
+            return scalar
+        scalar_t = torch.tensor(scalar, dtype=torch.float, device=args.device) / torch.distributed.get_world_size()
+        torch.distributed.all_reduce(scalar_t, op=torch.distributed.ReduceOp.SUM)
+        return scalar_t.item()
+
+    def evaluator(self, data, epoch):
+        if self.conf.distributed:
+            self.valid_sampler.set_epoch(1)
+        metrics = {}
+        _sum = 0
+        num_examples = 0
+        loss_fn = torch.nn.CrossEntropyLoss(ignore_index=-1)
+
+        for batch in data:
+
+            self.model.eval()
+            with torch.no_grad():
+                input_ids, token_type_ids, lm_labels = tuple(
+                    input_tensor.to(self.conf.device) for input_tensor in batch)
+                # logger.info(tokenizer.decode(input_ids[0, -1, :].tolist()))
+                lm_logits, *_ = self.model(input_ids, token_type_ids=token_type_ids)
+                lm_logits_flat_shifted = lm_logits[..., :-1, :].contiguous().view(-1, lm_logits.size(-1))
+                lm_labels_flat_shifted = lm_labels[..., 1:].contiguous().view(-1)
+
+            average_loss = loss_fn(lm_logits_flat_shifted, lm_labels_flat_shifted)
+            _sum += average_loss.item() * len(lm_labels_flat_shifted)
+            num_examples += len(lm_labels_flat_shifted)
+
+        metrics['nll'] = _sum / num_examples
+        metrics['average_nll'] = self.average_distributed_scalar([metrics['nll']], self.conf)
+        metrics['average_ppl'] = math.exp(*metrics['average_nll'])
+        self.pbar.write("Validation: %s" % pformat(metrics))
+        # todo print log
+        # todo checkpoint
